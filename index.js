ea5caedd51f7dc19206fc179bd0c4c5950893387
IngabireClarisse <clarriseingabire69@gmail.com>
2022-08-08 16:18:07 +0200

    the first jscode

---
+++ index.js
@@ -0,0 +1,12 @@
+let obj = "my name";
+console.log(obj);
+//function to display the full name
+
+const myname = function (fname, lname) {
+    let fullname;
+    let Fname = "olivier";
+    let Lname = "Nsenga";
+    fullname = Fname + Lname;
+    return fullname;
+}
+console.log(myname("fullname"));
